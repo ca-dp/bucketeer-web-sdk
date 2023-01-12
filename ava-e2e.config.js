cc0bf9194d6c90dc7d390081b979e0177c5e611d
Alessandro Yuichi Okimoto <yuichijpn@gmail.com>
2023-01-12 17:49:00 +0900

    ci: add e2e test

--- ava-e2e.config.js
+++ ava-e2e.config.js
@@ -8,7 +8,7 @@ export default {
   require: ['./tools/ava/setupWindow'],
   files: ['__e2e/__test__/*.js'],
   environmentVariables: {
-    HOST: '<HOST>', // replace this. e.g. https://api-dev.bucketeer.jp:443
+    HOST: '<HOST>', // replace this. e.g. https://example.com:443
     TOKEN: '<TOKEN>', // replace this.
   },
 };
